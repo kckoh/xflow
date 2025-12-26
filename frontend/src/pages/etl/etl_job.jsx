ec3fd030c5f2f9486fa18863b0f30e113a3b42cc
kckoh <kyungcheolkoh@gmail.com>
2025-12-26 16:38:02 +0900

    chore: setup basic structure to create the react pages

---
+++ frontend/src/pages/etl/etl_job.jsx
@@ -0,0 +1,6 @@
+import { useState } from "react";
+import { Play, Code, FileText, BarChart3, Plus, Info } from "lucide-react";
+
+export default function ETLJobPage() {
+  return <div>Hello etl job</div>;
+}
