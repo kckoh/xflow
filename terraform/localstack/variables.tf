ba55d9c95c7d672bc7cdae9f10680c4d3583188f
kckoh <kyungcheolkoh@gmail.com>
2025-12-30 17:10:09 +0900

    fix: remove all backend for aws except for s3

--- terraform/localstack/variables.tf
+++ terraform/localstack/variables.tf
@@ -22,58 +22,9 @@ variable "localstack_endpoint" {
   default     = "http://localhost:4566"
 }
 
-variable "vpc_cidr" {
-  description = "CIDR block for VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "public_subnet_cidr" {
-  description = "CIDR block for public subnet"
-  type        = string
-  default     = "10.0.1.0/24"
-}
-
-variable "private_subnet_cidr" {
-  description = "CIDR block for private subnet"
-  type        = string
-  default     = "10.0.2.0/24"
-}
-
-variable "availability_zone" {
-  description = "Availability zone"
-  type        = string
-  default     = "ap-northeast-2a"
-}
-
 variable "environment" {
   description = "Environment name"
   type        = string
   default     = "development"
-}
-
-variable "project_name" {
-  description = "Project name"
-  type        = string
-  default     = "xflow"
-}
-
-# Database Credentials
-variable "rds_username" {
-  description = "RDS master username"
-  type        = string
-  default     = "admin"
-}
-
-variable "rds_password" {
-  description = "RDS master password"
-  type        = string
-  sensitive   = true
-}
-
-variable "rds_database_name" {
-  description = "RDS database name"
-  type        = string
-  default     = "xflowdb"
 }
 
